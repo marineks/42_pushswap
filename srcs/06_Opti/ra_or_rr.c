dc5e51dd09a06938f343ca5f4ec7c90e6994edab
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 11:15:35 +0100

    42 norm OK

--- srcs/06_Opti/ra_or_rr.c
+++ srcs/06_Opti/ra_or_rr.c
@@ -6,15 +6,15 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 19:41:21 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 19:42:03 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 11:15:00 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pushswap.h"
 
-void    ra_or_rr(t_data *data)
+void	ra_or_rr(t_data *data)
 {
-	t_list *further;
+	t_list	*further;
 
 	if (data->len_b >= 2)
 	{
