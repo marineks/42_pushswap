b5013a051cfcc361dcd646e82a95af2a0ac7e0c1
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 10:58:15 +0100

    42 norm OK

--- srcs/03_Operations/rotate_op.c
+++ srcs/03_Operations/rotate_op.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:35:37 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 18:24:11 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 10:58:03 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,16 @@ rr : ra and rb at the same time
 
 void	rotate_a(t_data *data)
 {
-	t_list *head;
-	t_list *last;
+	t_list	*head;
+	t_list	*last;
 
 	if (data->len_a > 1)
 	{
 		head = data->stack_a;
 		last = ft_lstlast(data->stack_a);
-
 		data->stack_a = data->stack_a->next;
 		last->next = head;
 		head->next = NULL;
-
 		if (data->print == 1)
 			ft_putstr_fd("ra\n", 1);
 	}
@@ -41,19 +39,16 @@ void	rotate_a(t_data *data)
 
 void	rotate_b(t_data *data)
 {
-	t_list *head;
-	t_list *last;
+	t_list	*head;
+	t_list	*last;
 
 	if (data->len_b > 1)
 	{
-
 		head = data->stack_b;
 		last = ft_lstlast(data->stack_b);
-
 		data->stack_b = data->stack_b->next;
 		last->next = head;
 		head->next = NULL;
-		
 		if (data->print == 1)
 			ft_putstr_fd("rb\n", 1);
 	}
