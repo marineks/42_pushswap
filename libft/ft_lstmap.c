7b317b4e3129ee49af73713d161fec673fb1c76b
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 14:52:17 +0100

    deleted my nice macros to comply with 42 norm

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 17:23:42 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/26 11:32:58 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 14:51:21 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, long int (*f)(long int), void (*del)(long int))
 {
 	t_list	*cpy_list;
 	t_list	*new;
