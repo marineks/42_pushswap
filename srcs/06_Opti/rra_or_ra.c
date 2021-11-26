3ceac704c44045823270cc53b769746684c3a69a
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 11:17:56 +0100

    42 norm OK

--- srcs/06_Opti/rra_or_ra.c
+++ srcs/06_Opti/rra_or_ra.c
@@ -6,17 +6,17 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 19:42:13 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 19:42:54 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 11:17:47 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pushswap.h"
 
-void    rra_or_ra(t_data* data, t_list *tmp)
+void	rra_or_ra(t_data *data, t_list *tmp)
 {
 	while (STACK_A->number != tmp->number)
-	{  
-		if (find_index(tmp, tmp->number) > ft_lstsize(STACK_A)/2)
+	{
+		if (find_index(tmp, tmp->number) > ft_lstsize(STACK_A) / 2)
 		{
 			rra_or_rrr(data);
 		}
