b44635590faed42e0f0c964bd1d602a478d27c70
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 10:13:17 +0100

    42 norm OK

--- srcs/02_Format/display_stack.c
+++ srcs/02_Format/display_stack.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 18:08:11 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 20:07:19 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 10:08:43 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ void	display_stack(t_data *data, char which_stack)
 {
 	int i;
 	t_list *tmp;
-	// t_list *tmp_b;
 
 	i = 0;
 	tmp = data->stack_a;
-	// tmp_b = data->stack_b;
 	printf("  |====== STACK %c ======|  \n", which_stack);
 	printf("  |                     |  \n");
 	if (which_stack == 'B')
--- srcs/02_Format/format_list.c
+++ srcs/02_Format/format_list.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 14:39:35 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 19:37:50 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 10:13:07 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ void	initialize(t_data *data, int argc)
 
 void	put_in_stack_a(char **arg, t_data *data)
 {
-	int 		i;
-	long int 	nb;
+	int			i;
+	long int	nb;
 
 	i = 1;
 	while (arg[i])
 	{
-		if (is_digit(arg[i]) == SUCCESS &&
-			check_quotes(arg[i]) == SUCCESS &&
-			check_int_size(arg[i]) == SUCCESS)
+		if (is_digit(arg[i]) == SUCCESS
+			&& check_quotes(arg[i]) == SUCCESS
+			&& check_int_size(arg[i]) == SUCCESS)
 		{
 			nb = ft_atol(arg[i]);
 			ft_lstadd_back(&data->stack_a, ft_lstnew(nb));
@@ -44,6 +44,6 @@ void	put_in_stack_a(char **arg, t_data *data)
 			free_everything(data);
 			exit(0);
 		}
-	i++;
+		i++;
 	}	
 }
