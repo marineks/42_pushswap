96993c044a0797ff85597e9c30de43bf5d62685a
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 12:04:23 +0100

    42 norm OK

--- srcs/05_Utils/find_max.c
+++ srcs/05_Utils/find_max.c
@@ -6,17 +6,17 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 14:06:54 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 20:05:55 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 12:03:43 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pushswap.h"
 
-int find_max(t_list *stack)
+int	find_max(t_list *stack)
 {
-	t_list 		*tmp;
-	int	max;
-	
+	t_list	*tmp;
+	int		max;
+
 	tmp = stack;
 	max = stack->number;
 	while (tmp != NULL)
