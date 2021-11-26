061a0f714ca537d4d1991bc17325930bd652587c
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 08:59:28 +0100

    42 norm OK

--- srcs/04_Algorithms/big_sort.c
+++ srcs/04_Algorithms/big_sort.c
@@ -6,27 +6,25 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 13:30:30 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 19:51:01 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 08:59:11 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pushswap.h"
 
-
 void	apply_big_num_solver(t_data *data, int nbr)
 {
-	int count;
-	 
+	int	count;
+
 	count = 1;
 	TOTAL_SIZE = ft_lstsize(COPY);
 	sort_copy(COPY);
-
 	while (count < nbr)
 	{
-		CHUNK = find_value(COPY, (TOTAL_SIZE/nbr * count));
+		CHUNK = find_value(COPY, (TOTAL_SIZE / nbr * count));
 		push_chunk_to_B(data, CHUNK);
 		count++;
 	}
 	sort_last_chunk(data);
 	push_everything_back_to_A(data);
-}  
+}
