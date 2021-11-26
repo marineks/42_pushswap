3daae6b0f98cad795407880b53e7ebc5080702f9
Marine SANJUAN <marine@sanjuan.eu>
2021-11-26 09:29:20 +0100

    42 norm OK

--- srcs/01_Errors/check_quotes.c
+++ srcs/01_Errors/check_quotes.c
@@ -6,27 +6,27 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 17:05:05 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/11/25 17:11:46 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/11/26 09:29:08 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pushswap.h"
 
-int check_quotes(char *arg)
+int	check_quotes(char *arg)
 {
-    int i;
-    int count;
+	int	i;
+	int	count;
 
-    i = 0;
-    count = 0;
-    while (arg[i])
-    {
-        if (arg[i] == '\"')
-            count++;
-        i++;
-    }
-    if (count == 2)
-        return (FAILURE);
-    else
-        return (SUCCESS);
+	i = 0;
+	count = 0;
+	while (arg[i])
+	{
+		if (arg[i] == '\"')
+			count++;
+		i++;
+	}
+	if (count == 2)
+		return (FAILURE);
+	else
+		return (SUCCESS);
 }
