5b6b8b0d58575f96f571d9272fdc5175bf264684
Marine SANJUAN <marine@sanjuan.eu>
2021-12-02 11:46:02 +0100

    42 norm OK

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 17:04:25 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/12/01 16:32:10 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/12/02 11:45:36 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@
 # include <limits.h>
 
 // CHECKER.c
-void    get_instructions(t_data *data);
-void    do_operation(char *str, t_data *data);
+void	get_instructions(t_data *data);
+void	do_operation(char *str, t_data *data);
 
 // DOUBLE_OP_EXCEPTION.c
 void	reverse_rrr(t_data *data);
